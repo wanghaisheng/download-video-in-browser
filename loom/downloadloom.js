f7dce9e392533eefcfcaeb612a3d82a0763c311c
wanghaisheng <edwin_uestc@163.com>
2023-12-07 16:10:17 +0800

    fix loom

---
+++ loom/downloadloom.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+const fs = require('fs');
+const https = require('https');
+
+const fetchLoomDownloadUrl = async (id) => {
+  try {
+    const { data } = await axios.post(`https://www.loom.com/api/campaigns/sessions/${id}/transcoded-url`);
+    return { success: true, message: 'Fetched data', data: data.url }
+  } catch (error) {
+    return { success: false, message: 'There are some issues while fetching loom download URL.', data: null }
+  }
+};
+
+const downloadLoomVideo = (url, filename) => {
+
+  try {
+    const file = fs.createWriteStream(filename);
+    const request = https.get(url, function (response) {
+      response.pipe(file);
+    });
+
+    return { success: true, message: 'Video has been downloaded successfully.' }
+  } catch (error) {
+    return { success: false, message: 'There are some issues while downloading video.' }
+  }
+
+};
+
+const isLoomVideoUrl = (url) => {
+  const loomDomain = 'www.loom.com';
+  const trimmedUrl = url.trim();
+  return trimmedUrl.startsWith('https://') && trimmedUrl.includes(loomDomain);
+}
+
+const getId = (videoUrl) => {
+  const id = videoUrl.split('/').pop().split("?").shift();
+  return id
+}
+
+const main = async (videoUrl) => {
+  const id = getId(videoUrl);
+  const urlResponse = await fetchLoomDownloadUrl(id);
+  if (urlResponse.success) {
+    const filename = `${id}.mp4`;
+    const response = downloadLoomVideo(urlResponse.data, filename);
+    return response;
+  }
+
+  return urlResponse;
+};
+
+module.exports = {
+  main, isLoomVideoUrl
+}
---
+++ loom/loom-down.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { main, isLoomVideoUrl } = require('./downloadloom');
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+app.post('/download-video', async (req, res) => {
+    const url = req.body.url;
+
+    if (!url || (url && !isLoomVideoUrl(url))) {
+        return res.json({ success: false, message: "Please provide a loom video URL!" });
+    }
+
+    const response = await main(url);
+    return res.json(response);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
